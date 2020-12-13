84b6db1a8933e9aa42eaf31aa063ef5ab773c4fc
Andrew Wood <andrewwood90@hotmail.com>
2020-12-13 09:48:39 +0000

    two tests passed

--- cooking_academy.py
+++ cooking_academy.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
+cooking_academy = Flask(__name__)
+
+
+@cooking_academy.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    cooking_academy.run(debug=True)
---
+++ db/cooking_academy.sql
@@ -0,0 +1,25 @@
+DROP TABLE bookings;
+DROP TABLE lessons;
+DROP TABLE members;
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+
+);
+
+
+CREATE TABLE lessons (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+
+);
+
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    member_id INT REFERENCES members(id) ON DELETE CASCADE,
+    lesson_id INT REFERENCES lessons(id) ON DELETE CASCADE
+
+);
+
