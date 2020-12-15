1efe8deca431f18a79b0bc6e73c1d6ab2214e709
Andrew Wood <andrewwood90@hotmail.com>
2020-12-15 10:30:44 +0000

    cooking_academy.py load order fixed

--- cooking_academy.py
+++ cooking_academy.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 from controllers.member_controller import members_blueprint
 from controllers.lessons_controller import lessons_blueprint
 from controllers.bookings_controller import bookings_blueprint
 
 
+
+
+
 cooking_academy = Flask(__name__)
 
 
+
+
+if __name__ == '__main__':
+    cooking_academy.run(debug=True)
 
 cooking_academy.register_blueprint(members_blueprint)
 cooking_academy.register_blueprint(lessons_blueprint)
@@ -16,6 +23,3 @@ cooking_academy.register_blueprint(bookings_blueprint)
 @cooking_academy.route('/')
 def home():
     return render_template('index.html')
-
-if __name__ == '__main__':
-    cooking_academy.run(debug=True)
